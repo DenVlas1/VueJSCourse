e1dfc0175d396ece1b782da6d5297e4350612223
DenVlas1 <vlas2133@gmail.com>
2025-05-05 00:14:14 +0300

    Виконане завдання 3

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,47 @@
 <!-- src/App.vue -->
 <template>
-<div>
-  <b-navbar :type="dark" :variant="dark">
-    <b-container>
-      <b-navbar-brand>Завдання №3</b-navbar-brand>
-      <b-navbar-nav class="me-auto">
-        <b-nav-item
-          v-for="(page, index) in pages"
-          :key="index"
-          :active="activePage === index"
-          @click.prevent="activePage = index">
-          {{ page.link.text }} </b-nav-item>
-           </b-navbar-nav>
-      <b-button class="btn" @click="ChangeTheme">Змінити колір</b-button>
-    </b-container>
-  </b-navbar>
-  <page-viewer
-      :page="pages[activePage]">
-  </page-viewer>
-</div>
+  <div>
+    <b-navbar :type="dark" :variant="dark">
+      <b-container>
+        <b-navbar-brand>Завдання №3</b-navbar-brand>
+        <b-navbar-nav class="me-auto">
+          <!--          виведення усіх значень зі списку сторінок-->
+          <b-nav-item
+            v-for="(page, index) in pages"
+            :key="index"
+            :active="activePage === index"
+            @click.prevent="activePage = index"
+          >
+            {{ page.link.text }}
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-container>
+    </b-navbar>
+    <page-viewer :page="pages[activePage]"> </page-viewer>
+  </div>
 </template>
 
 <script>
-import PageViewer from '@/components/PageViewer.vue'
+import PageViewer from "@/components/PageViewer.vue";
 export default {
-  components: {PageViewer},
+  components: { PageViewer },
   data() {
     return {
       activePage: 0,
-      theme: 'dark',
+      theme: "dark",
       pages: [
         {
-          link: { text: "Home", url: "index.html" },
+          link: { text: "Home" },
           pageTitle: "HomePage",
           content: "test1",
         },
         {
-          link: { text: "About", url: "about.html" },
+          link: { text: "About" },
           pageTitle: "About",
           content: "test2",
-        },
-        {
-          link: { text: "Contact", url: "contact.html" },
-          pageTitle: "Contact",
-          content: "test3",
         },
       ],
     };
   },
-
 };
 </script>
