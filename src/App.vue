606bb1e3395c472c8522f37597ca7cd777e9e12b
DenVlas1 <vlas2133@gmail.com>
2025-05-11 19:11:02 +0300

    Виконане завдання 4

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,18 @@
 <!-- src/App.vue -->
 <template>
-  <b-container>
-    <b-navbar type="dark" variant="dark">
+  <b-container class="text-center my-5">
+    <user-card name="Вадим" :age="22" :is-active="true"></user-card>
+    <user-card name="Андрій" :age="32" :is-active="true"></user-card>
+    <user-card name="Никита" :age="50" :is-active="false"></user-card>
+    <user-card></user-card>
 
-        <b-navbar-brand>Завдання №3</b-navbar-brand>
-        <b-navbar-nav class="me-auto">
-          <!--          виведення усіх значень зі списку сторінок-->
-          <b-nav-item
-            v-for="(page, index) in pages"
-            :key="index"
-            :active="activePage === index"
-            @click.prevent="activePage = index"
-          >
-            {{ page.link.text }}
-          </b-nav-item>
-        </b-navbar-nav>
-    </b-navbar>
-      <page-viewer :page="pages[activePage]"> </page-viewer>
   </b-container>
-
-
 </template>
 
 <script>
-import PageViewer from "@/components/PageViewer.vue";
+import UserCard from "@/components/UserCard.vue";
+
 export default {
-  components: { PageViewer },
-  data() {
-    return {
-      activePage: 0,
-      theme: "dark",
-      pages: [
-        {
-          link: { text: "Home" },
-          pageTitle: "HomePage",
-          content: "test1",
-        },
-        {
-          link: { text: "About" },
-          pageTitle: "About",
-          content: "test2",
-        },
-      ],
-    };
-  },
+  components: { UserCard },
 };
 </script>
