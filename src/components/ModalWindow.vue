10bf7e500f3eab20ad06fed7dd21628a2d9864b8
DenVlas1 <vlas2133@gmail.com>
2025-05-16 02:02:22 +0300

    Виконане завдання 7

---
+++ src/components/ModalWindow.vue
@@ -0,0 +1,48 @@
+<script >
+export default {
+
+  props:{
+    carNumber: Number,
+    driverName: String,
+    carWeight: Number,
+    openModal: Boolean
+  },
+  computed:{
+    modalVisible:{
+      get(){
+        return this.openModal;
+      },
+      set(val){
+        this.$emit('update:openModal',val);
+      }
+    }
+  },
+  methods:{
+
+emitConfirm(){
+  const payload={
+    carNumber:this.carNumber,
+    driverName:this.driverName,
+    carWeight:this.carWeight,
+
+  }
+  this.$emit('confirm',payload);
+},
+    emitCancel(){
+       this.$emit('cancel');
+    }
+  }
+}
+</script>
+
+<template>
+  <b-container>
+<b-modal @ok="emitConfirm"  @cancel="emitCancel" v-model="modalVisible" title="Підтвердження">
+<h1>Прийняти зміни для машини {{carNumber}} з водієм {{ driverName }} та вагою {{ carWeight }}?</h1>
+</b-modal>
+  </b-container>
+</template>
+
+<style scoped>
+
+</style>
